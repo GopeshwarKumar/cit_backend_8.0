cc351dc8a85c860155ebd0415320d62df40c7e23
Gopeshwar kumar <150578464+GopeshwarKumar@users.noreply.github.com>
2025-02-01 09:28:04 +0530

    Update CitUserModels.js

--- Models/CitUserModels.js
+++ Models/CitUserModels.js
@@ -1,9 +1,6 @@
 const mongoose=require("mongoose");
 
-mongoose.connect(process.env.MODELS_MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MODELS_MONGO_URL).then(() => {
     console.log('Connected to DB');
 }).catch(err => {
     console.log('DB connection error: ', err);
